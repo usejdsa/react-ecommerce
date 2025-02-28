42812345efe6a17436e2704f472974bfc71b8dbb
usejdsa <157837649+usejdsa@users.noreply.github.com>
2025-02-28 21:41:56 +0100

    init

--- src/components/Shared/Footer.js
+++ src/components/Shared/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer className="bg-gray-800 text-white text-center p-4 mt-10">
+        <footer className="bg-gray-800 text-white text-center p-4 mt-10  ">
             <p>©2025 usejdSa. All rights reserved.</p>
         </footer>
     )
